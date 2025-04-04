3f38621e0bf00f3f28395c519ea1431d0dde62bc
Lorenz1212 <lorenzcabreros@gmail.com>
2025-04-05 03:20:16 +0800

    update

---
+++ connection/connect.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+error_reporting(0);
+// ini_set('display_errors', 1);
+// $host = "localhost";
+// $username = "root";
+// $password = "";
+// $database = "brgy_info";
+
+
+$host = "localhost";
+$username = "u856582098_bis_v2"; 
+$password = "2R=[mzmO8t&";
+$database = "u856582098_bis_v2";
+
+
+
+// Create connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
