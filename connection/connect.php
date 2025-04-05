3558759f8536cc0aa1a951858aa8cf2bc60b9fa0
Lorenz1212 <lorenzcabreros@gmail.com>
2025-04-06 02:26:44 +0800

    Update connect.php

--- connection/connect.php
+++ connection/connect.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+// ini_set('display_errors', 1);
 // $host = "localhost";
 // $username = "root";
 // $password = "";
